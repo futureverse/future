f642edcc28214e8874bb4dcda4d964bc8a9cb09e
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-03-07 23:48:48 -0800

    Add prune_debug()

--- R/utils-prune_pkg_code.R
+++ R/utils-prune_pkg_code.R
@@ -14,10 +14,38 @@ prune_call <- function(expr, name) {
   NULL
 }
 
+prune_debug <- function(expr) {
+  if (!is.call(expr)) 
+    return(expr)
+  expr <- unclass(expr)
+  fcn <- expr[[1]]
+  if (!is.symbol(fcn)) 
+    return(expr)
+    
+  ## if (debug) { ... }
+  if (as.character(fcn) == "if") {
+    cond <- expr[[2]]
+    if (!is.symbol(cond)) 
+      return(expr)
+    if (as.character(cond) != "debug")
+      return(expr)
+    expr <- NULL
+  } else if (as.character(fcn) == "<-") {
+    lhs <- expr[[2]]
+    if (!is.symbol(lhs)) 
+      return(expr)
+    if (as.character(lhs) != "debug")
+      return(expr)
+    expr <- NULL
+  }
+  expr
+}
+
 prune_fcns <- function(expr) {
   expr <- prune_call(expr, name = "stop_if_not")
   expr <- prune_call(expr, name = "with_assert")
   expr <- prune_call(expr, name = "assert_no_positional_args_but_first")
+  expr <- prune_debug(expr)
   expr
 }
 
@@ -40,7 +68,6 @@ prune_fcn <- function(name, envir) {
 #' @importFrom globals walkAST
 prune_pkg_code <- function(env = topenv(parent.frame())) {
   res <- lapply(names(env), FUN = prune_fcn, envir = env)
-
   env <- environment(plan)
   res <- lapply(names(env), FUN = prune_fcn, envir = env)
 }
