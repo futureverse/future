d07ed2564bb92eb8a26765bfa09fa17e5d69e189
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-12-04 13:27:46 -0800

    Make it possible to ignore deprecation warnings on future(..., earlySignal = ...) for the time being

--- R/009.deprecation.R
+++ R/009.deprecation.R
@@ -1,5 +1,7 @@
 deprecate_future_earlySignal <- function(earlySignal) {
   action <- getOption("future.future.earlySignal", "deprecated")
+  action <- match.arg(action, choices = c("deprecated", "defunct", "ignore"))
+  if (action == "ignore") return()
   dfcn <- if (action == "deprecated") .Deprecated else .Defunct
   dfcn(msg = sprintf("future() no longer takes argument 'earlySignal'. Attempts to use it is ignored, but will eventually become an error: earlySignal = %s", deparse(earlySignal)[1]))
 }
