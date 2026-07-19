2892e40f506d35a02d08639ce6a00839a13f82b0
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2026-07-19 13:09:59 +0200

    TESTS: Disable most of registerClusterTypes() testing on R (< 4.5.0)

--- inst/testme/test-registerClusterTypes.R
+++ inst/testme/test-registerClusterTypes.R
@@ -10,19 +10,20 @@ message("*** registerClusterTypes() ...")
 ## The 'FUTURE' cluster type is registered when the package is loaded
 stopifnot(identical(future:::FUTURE, "future::FUTURE"))
 
-## Calling it again is a no-op, because it has already been done
 env <- environment(registerClusterTypes)
-stopifnot(isTRUE(env[["done"]]))
-stopifnot(is.null(registerClusterTypes()))
-
-## Force it to run its body once more, which must be idempotent
-env[["done"]] <- FALSE
-registerClusterTypes()
-stopifnot(isTRUE(env[["done"]]) ||
-          !exists("registerClusterType", envir = getNamespace("parallel")))
 
 ## On R (>= 4.5.0) the type is registered with parallel
 if (exists("registerClusterType", envir = getNamespace("parallel"))) {
+  ## Calling it again is a no-op, because it has already been done
+  stopifnot(isTRUE(env[["done"]]))
+  stopifnot(is.null(registerClusterTypes()))
+  
+  ## Force it to run its body once more, which must be idempotent
+  env[["done"]] <- FALSE
+  registerClusterTypes()
+  stopifnot(isTRUE(env[["done"]]) ||
+            !exists("registerClusterType", envir = getNamespace("parallel")))
+
   stopifnot(isTRUE(env[["done"]]))
 } else {
   ## On older R, registerClusterTypes() returns before setting 'done'
